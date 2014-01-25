87757b98ac4ea6ae2e786d7a8a7b9567867a76ac
Han Xu <hgcrpd@gmail.com>
2014-01-25 18:43:58 +0800

    Tests, but they don't work

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
+from flask import Flask, jsonify, request
+import models
 
-from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/api')
+def api():
+    print request.args
+    return jsonify(message='Nothing here')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
