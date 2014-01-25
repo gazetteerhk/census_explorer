999370c0d03a783a87a3522324e3f264fbefeef5
Han Xu <hgcrpd@gmail.com>
2014-01-26 00:24:46 +0800

    make test server avialable to all

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -62,12 +62,12 @@ module.exports = function (grunt) {
       options: {
         port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
         options: {
-          open: true,
+          open: false,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
