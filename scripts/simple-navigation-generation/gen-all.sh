bcba5e78f74ee7e0020102e895bdf61b18ff6c78
HU Pili <hpl1989@gmail.com>
2014-01-28 18:02:44 +0000

    upgrade simple navigation for fast data check

--- scripts/simple-navigation-generation/gen-all.sh
+++ scripts/simple-navigation-generation/gen-all.sh
@@ -2,19 +2,19 @@
 
 set -xe
 
+if [[ $# == 1 ]] ; then
+	root="$1"
+else
+	echo "usage: $0 {root}"
+	exit 255
+fi
+
 rm -f cmd.sh
 touch cmd.sh
 
-root="../data-clean"
-echo ./gen-one-dir.sh $root >> cmd.sh
-
-for area in `ls -1 $root | grep -v 'index.html'`
+for dir in `find $root -type d`
 do
-	echo ./gen-one-dir.sh $root/$area >> cmd.sh
-	for sheet in `ls -1 $root/$area | grep -v 'index.html'`
-	do
-		echo ./gen-one-dir.sh $root/$area/$sheet >> cmd.sh
-	done
+	echo ./gen-one-dir.sh $dir >> cmd.sh
 done
 
 xargs -P20 -a cmd.sh -i sh -c '{}'
