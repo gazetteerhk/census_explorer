0f6748ec581b1b2fe7859c484f8a4989bf1fc6b0
HU Pili <hpl1989@gmail.com>
2014-01-29 08:50:57 +0000

    fix missing unicode modifier

--- scripts/table_meta_data.py
+++ scripts/table_meta_data.py
@@ -1,18 +1,18 @@
 TABLE_META_DATA = [
   {
     "E": "Ethnicity", 
-    "S": "\u79cd\u65cf", 
+    "S": u"\u79cd\u65cf", 
     "table_id": 0, 
-    "T": "\u7a2e\u65cf",
+    "T": u"\u7a2e\u65cf",
     "name": "Ethnicity",
     "header": ["A6","E6"],
     "body": ["A7","E13"]
   }, 
   {
     "E": "Usual Language", 
-    "S": "\u60ef\u7528\u8bed\u8a00", 
+    "S": u"\u60ef\u7528\u8bed\u8a00", 
     "table_id": 1, 
-    "T": "\u6163\u7528\u8a9e\u8a00",
+    "T": u"\u6163\u7528\u8a9e\u8a00",
     "name": "Usual Language",
     "header": ["A18","E18"],
     "body": ["A19","E24"]
@@ -20,173 +20,187 @@ TABLE_META_DATA = [
   }, 
   {
     "E": "Marital Status", 
-    "S": "\u5a5a\u59fb\u72b6\u51b5", 
+    "S": u"\u5a5a\u59fb\u72b6\u51b5", 
     "table_id": 2, 
-    "T": "\u5a5a\u59fb\u72c0\u6cc1",
+    "T": u"\u5a5a\u59fb\u72c0\u6cc1",
     "name": "Marital Status",
     "header": ["A28","E28"],
     "body": ["A29","E34"]
   }, 
   {
     "E": "Educational Attainment", 
-    "S": "\u6559\u80b2\u7a0b\u5ea6", 
+    "S": u"\u6559\u80b2\u7a0b\u5ea6", 
     "table_id": 3, 
-    "T": "\u6559\u80b2\u7a0b\u5ea6",
+    "T": u"\u6559\u80b2\u7a0b\u5ea6",
     "name": "Educational Attainment (Highest Level Attended)",
     "header": ["A43","E43"],
     "body": ["A44","E53"]
   }, 
   {
     "E": "Economic Activity Status", 
-    "S": "\u7ecf\u6d4e\u6d3b\u52a8\u8eab\u5206", 
+    "S": u"\u7ecf\u6d4e\u6d3b\u52a8\u8eab\u5206", 
     "table_id": 4, 
-    "T": "\u7d93\u6fdf\u6d3b\u52d5\u8eab\u5206",
+    "T": u"\u7d93\u6fdf\u6d3b\u52d5\u8eab\u5206",
     "name": "Economic Activity Status",
     "header": ["A61","E61"],
     "body": ["A62","E71"]
   }, 
   {
     "E": "Monthly Income from Main Employment (HK$)", 
-    "S": "\u6bcf\u6708\u4e3b\u8981\u804c\u4e1a\u6536\u5165\uff08\u6e2f\u5143\uff09", 
+    "S": u"\u6bcf\u6708\u4e3b\u8981\u804c\u4e1a\u6536\u5165\uff08\u6e2f\u5143\uff09", 
     "table_id": 5, 
-    "T": "\u6bcf\u6708\u4e3b\u8981\u8077\u696d\u6536\u5165\uff08\u6e2f\u5143\uff09",
+    "T": u"\u6bcf\u6708\u4e3b\u8981\u8077\u696d\u6536\u5165\uff08\u6e2f\u5143\uff09",
     "name": "Monthly Income from Main Employment (HK$)",
     "header": ["A77","E77"],
     "body": ["A78","E103"]
   }, 
   {
     "E": "Household Composition", 
-    "S": "\u4f4f\u6237\u7ed3\u6784", 
+    "S": u"\u4f4f\u6237\u7ed3\u6784", 
     "table_id": 6, 
-    "T": "\u4f4f\u6236\u7d50\u69cb",
+    "T": u"\u4f4f\u6236\u7d50\u69cb",
     "name": "Household Composition",
     "header": ["A114","E114"],
     "body": ["A115","E126"]
   }, 
   {
     "E": "Household Size", 
-    "S": "\u4f4f\u6237\u4eba\u6570", 
+    "S": u"\u4f4f\u6237\u4eba\u6570", 
     "table_id": 7, 
-    "T": "\u4f4f\u6236\u4eba\u6578",
+    "T": u"\u4f4f\u6236\u4eba\u6578",
     "name": "Household Size",
     "header": ["A129","E129"],
     "body": ["A130","E135"]
   }, 
   {
     "E": "Type of Housing", 
-    "S": "\u623f\u5c4b\u7c7b\u578b", 
+    "S": u"\u623f\u5c4b\u7c7b\u578b", 
     "table_id": 8, 
-    "T": "\u623f\u5c4b\u985e\u578b",
+    "T": u"\u623f\u5c4b\u985e\u578b",
     "name": "Type of Housing",
     "header": ["A146","E146"],
     "body": ["A147","E151"]
   }, 
   {
     "E": "Tenure of Accommodation", 
-    "S": "\u5c45\u6240\u79df\u4f4f\u6743", 
+    "S": u"\u5c45\u6240\u79df\u4f4f\u6743", 
     "table_id": 9, 
-    "T": "\u5c45\u6240\u79df\u4f4f\u6b0a",
+    "T": u"\u5c45\u6240\u79df\u4f4f\u6b0a",
     "name": "Tenure of Accommodation",
     "header": ["A154","E154"],
     "body": ["A155","E162"]
   }, 
   {
     "E": "Type of Housing", 
-    "S": "\u623f\u5c4b\u7c7b\u578b", 
+    "S": u"\u623f\u5c4b\u7c7b\u578b", 
     "table_id": 10, 
-    "T": "\u623f\u5c4b\u985e\u578b",
+    "T": u"\u623f\u5c4b\u985e\u578b",
     "name": "Type of Housing",
     "header": ["A168","E168"],
     "body": ["A169","E175"]
   }, 
   {
     "E": "Area of Residence 5 Years Ago", 
-    "S": "\u4e94\u5e74\u524d\u5c45\u4f4f\u5730\u533a", 
+    "S": u"\u4e94\u5e74\u524d\u5c45\u4f4f\u5730\u533a", 
     "table_id": 11, 
-    "T": "\u4e94\u5e74\u524d\u5c45\u4f4f\u5730\u5340",
+    "T": u"\u4e94\u5e74\u524d\u5c45\u4f4f\u5730\u5340",
     "name": "Area of Residence 5 Years Ago",
     "header": ["A184","E184"],
     "body": ["A185","E193"]
   }, 
   {
     "E": "Age Group", 
-    "S": "\u5e74\u9f84\u7ec4\u522b", 
+    "S": u"\u5e74\u9f84\u7ec4\u522b", 
     "table_id": 12, 
-    "T": "\u5e74\u9f61\u7d44\u5225",
+    "T": u"\u5e74\u9f61\u7d44\u5225",
     "name": "Age Group",
     "header": ["H6","N6"],
     "body": ["H7","N24"]
   }, 
   {
     "E": "Age Group", 
-    "S": "\u5e74\u9f84\u7ec4\u522b", 
+    "S": u"\u5e74\u9f84\u7ec4\u522b", 
     "table_id": 13, 
-    "T": "\u5e74\u9f61\u7d44\u5225",
+    "T": u"\u5e74\u9f61\u7d44\u5225",
     "name": "Age Group - Proportion of Population",
     "header": ["H28","N28"],
     "body": ["H29","N33"]
   }, 
   {
     "E": "Place of Study", 
-    "S": "\u4e0a\u8bfe\u5730\u70b9", 
+    "S": u"\u4e0a\u8bfe\u5730\u70b9", 
     "table_id": 14, 
-    "T": "\u4e0a\u8ab2\u5730\u9ede",
+    "T": u"\u4e0a\u8ab2\u5730\u9ede",
     "name": "Place of Study",
     "header": ["H41","N41"],
     "body": ["H42","N47"]
   }, 
   {
     "E": "Place of Work", 
-    "S": "\u5de5\u4f5c\u5730\u70b9", 
+    "S": u"\u5de5\u4f5c\u5730\u70b9", 
     "table_id": 15, 
-    "T": "\u5de5\u4f5c\u5730\u9ede",
+    "T": u"\u5de5\u4f5c\u5730\u9ede",
     "name": "Place of Work",
     "header": ["H61","N61"],
     "body": ["H62","N70"]
   }, 
   {
     "E": "Occupation", 
-    "S": "\u804c\u4e1a", 
+    "S": u"\u804c\u4e1a", 
     "table_id": 16, 
-    "T": "\u8077\u696d",
+    "T": u"\u8077\u696d",
     "name": "Occupation",
     "header": ["H81","N81"],
     "body": ["H82","N91"]
   }, 
   {
     "E": "Industry", 
-    "S": "\u884c\u4e1a", 
+    "S": u"\u884c\u4e1a", 
     "table_id": 17, 
-    "T": "\u884c\u696d",
+    "T": u"\u884c\u696d",
     "name": "Industry",
     "header": ["H95","N95"],
     "body": ["H96","N107"]
   }, 
   {
     "E": "Monthly Domestic Household Income (HK$)", 
-    "S": "\u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u6536\u5165\uff08\u6e2f\u5143\uff09", 
+    "S": u"\u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u6536\u5165\uff08\u6e2f\u5143\uff09", 
     "table_id": 18, 
-    "T": "\u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u6536\u5165\uff08\u6e2f\u5143\uff09",
+    "T": u"\u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u6536\u5165\uff08\u6e2f\u5143\uff09",
     "name": "Monthly Domestic Household Income",
     "header": ["H118","N118"],
     "body": ["H119","N132"]
   }, 
   {
     "E": "Monthly Domestic Household Mortgage Payment and Loan Repayment (HK$)", 
-    "S": " \u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u6309\u63ed\u4f9b\u6b3e \u53ca\u501f\u8d37\u8fd8\u6b3e\uff08\u6e2f\u5143\uff09", 
+    "S": u"\u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u6309\u63ed\u4f9b\u6b3e \u53ca\u501f\u8d37\u8fd8\u6b3e\uff08\u6e2f\u5143\uff09", 
     "table_id": 19, 
-    "T": " \u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u6309\u63ed\u4f9b\u6b3e \u53ca\u501f\u8cb8\u9084\u6b3e\uff08\u6e2f\u5143\uff09",
+    "T": u"\u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u6309\u63ed\u4f9b\u6b3e \u53ca\u501f\u8cb8\u9084\u6b3e\uff08\u6e2f\u5143\uff09",
     "name": "Monthly Domestic Household Mortgage Payment and Loan Repayment",
     "header": ["H143","N143"],
     "body": ["H144","N155"]
   }, 
   {
     "E": "Monthly DomesticHousehold Rent (HK$)", 
-    "S": "\u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u79df\u91d1\uff08\u6e2f\u5143\uff09", 
+    "S": u"\u5bb6\u5ead\u4f4f\u6237\u6bcf\u6708\u79df\u91d1\uff08\u6e2f\u5143\uff09", 
     "table_id": 20, 
-    "T": "\u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u79df\u91d1\uff08\u6e2f\u5143\uff09",
+    "T": u"\u5bb6\u5ead\u4f4f\u6236\u6bcf\u6708\u79df\u91d1\uff08\u6e2f\u5143\uff09",
     "name": "Monthly Domestic Household Rent",
     "header": ["H162","N162"],
     "body": ["H163","N177"]
   }
 ]
+
+#
+
+_meta = []
+for d in TABLE_META_DATA:
+    names = {}
+    names['E'] = d.pop('E')
+    names['S'] = d.pop('S')
+    names['T'] = d.pop('T')
+    d['names'] = names
+    _meta.append(d)
+
+import pprint
+pprint.pprint(_meta)
