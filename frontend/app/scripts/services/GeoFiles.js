66405f21d6fe5ef200010d631c6bac61ec4e326f
Han Xu <hgcrpd@gmail.com>
2014-02-02 15:20:55 +0800

    Use HTTP cache in GeoFiles

--- frontend/app/scripts/services/GeoFiles.js
+++ frontend/app/scripts/services/GeoFiles.js
@@ -4,41 +4,27 @@ angular.module('frontendApp').factory('GeoFiles', ['$http', '$q', '$cacheFactory
    * Service that wraps the fetching and preparing of TopoJSON / GeoJSON files
    */
 
-  // TODO: Actually should use a resource here, and use the built in caching provided by $http
 
   var svc = {};
-  svc._cache = $cacheFactory('GeoFiles');
 
   svc.getAreas = function() {
     var deferred = $q.defer();
-    var cacheResult = svc._cache.get('areas');
 
-    if (_.isUndefined(cacheResult)) {
-      $http.get("scripts/geo/ca_polygon.topo.json").success(function(data, status) {
-        var geo_data = topojson.feature(data, data.objects['ca_polygon.geo']);
-        svc._cache.put('areas', geo_data);
-        deferred.resolve(geo_data)
-      });
-    } else {
-      deferred.resolve(cacheResult);
-    }
+    $http.get("scripts/geo/ca_polygon.topo.json", {cache: true}).success(function(data, status) {
+      var geo_data = topojson.feature(data, data.objects['ca_polygon.geo']);
+      deferred.resolve(geo_data)
+    });
 
     return deferred.promise;
   };
 
   svc.getDistricts = function() {
     var deferred = $q.defer();
-    var cacheResult = svc._cache.get('districts');
 
-    if (_.isUndefined(cacheResult)) {
-      $http.get("scripts/geo/dc_polygon.topo.json").success(function(data, status) {
-        var geo_data = topojson.feature(data, data.objects['dc_polygon.geo']);
-        svc._cache.put('districts', geo_data);
-        deferred.resolve(geo_data)
-      });
-    } else {
-      deferred.resolve(cacheResult);
-    }
+    $http.get("scripts/geo/dc_polygon.topo.json", {cache: true}).success(function(data, status) {
+      var geo_data = topojson.feature(data, data.objects['dc_polygon.geo']);
+      deferred.resolve(geo_data)
+    });
 
     return deferred.promise;
   };
