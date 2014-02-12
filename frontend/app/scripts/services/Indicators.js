c939ef9f23a79b7d7cd0d355c3fdc9b2a496dd67
Han Xu <hgcrpd@gmail.com>
2014-02-12 07:45:27 -0800

    Scaffolding for an Indicators service -- configs for commonly used indicators

---
+++ frontend/app/scripts/services/Indicators.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*
+ * A collection of configurations for commonly used indicators
+ */
+angular.module('frontendApp').factory('Indicators', [function() {
+  /*
+   * Color Scales
+   *
+   * ColorBrewer provides scales for anything with less than 11 categories, but sometimes we need a bit more
+   * For example, the tables with income information have 12-15 categories each.
+   */
+
+  /*
+   * Parsers
+   *
+   * Parsers are callables that are given an API data object, and return a list of data objects that can be use by charts
+   */
+
+
+  /*
+   * Query Parameters
+   *
+   * Common query parameters
+   */
+
+}]);
