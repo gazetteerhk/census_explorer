ec07c274f594c29cd058735e96f419fc8186fea8
HU Pili <hpl1989@gmail.com>
2014-02-13 23:59:21 +0800

    working ng-table from examples

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('frontendApp', [
+    'ngTable',
     'ngCookies',
     'ngResource',
     'ngSanitize',
--- frontend/app/scripts/controllers/browser.js
+++ frontend/app/scripts/controllers/browser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // API browser
-angular.module('frontendApp').controller('BrowserCtrl', ['$scope', 'CensusAPI', function($scope, CensusAPI) {
+angular.module('frontendApp').controller('BrowserCtrl', ['$scope', 'CensusAPI', 'ngTableParams', function($scope, CensusAPI, ngTableParams) {
   // Selected options
   $scope.model = {};
   // Available options
@@ -32,4 +32,32 @@ angular.module('frontendApp').controller('BrowserCtrl', ['$scope', 'CensusAPI', 
   };
 
   $scope.refresh();
+
+    var data = [{name: "Moroni", age: 50},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 34},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 34},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 34},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 34}];
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+    }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+    });
+
 }]);
