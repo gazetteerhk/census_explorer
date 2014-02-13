4ab34195fb744564d244d2fcbe0fa5921bb2edfb
Han Xu <hgcrpd@gmail.com>
2014-02-13 07:55:35 -0800

    cleanup

--- frontend/app/scripts/controllers/test.js
+++ frontend/app/scripts/controllers/test.js
@@ -19,35 +19,12 @@ angular.module('frontendApp')
         $scope.mapConfig = $scope.selectedIndicator.config;
         $scope.areaData = d;
       });
-
-      /*
-      $http.get(url).success(function(data){
-        console.log(data);
-        //NOTE:
-        //    Do not plot by .value. They are just the min, max, median number.
-        //    What we want to plot is .row, which are categorical information.
-        //    The following code creates a general map from categories to numerical values.
-        var m = {};
-        for (var i=0; i < data.options.row.length; i++){
-          m[data.options.row[i]] = i
-        }
-        $scope.areaData =_.map(data.groups, function(area,k) {
-          var v = m[area.row[0]];
-          //TODO:
-          //    suggest data structure:
-          //    - area: area
-          //    - value: a numerical level for coloring purpose
-          //    - name: the human readable category name, e.g. '>= 20000'.
-          //            Can be used in legend.
-          return {area: k, value: v};
-        });
-      });
-      */
     };
 
     /*
      * Median / mode income related indicators
      */
+
     // 14 categories total
     var _medianMonthlyIncomeColors = _.clone(colorbrewer.Reds['7']).reverse().concat(colorbrewer.Greens['7']);
     var _medianMonthlyIncomeConfig = {
@@ -62,9 +39,6 @@ angular.module('frontendApp')
     };
 
     $scope.indicators = [
-//      {name: 'Total population', identifier: {table: 0, column: 'tab0_both', row: 'tab0_total'}},
-//      {name: 'Population of divorcees', identifier: {table: 2, column: 'e28_both', row: 'a32_divorced'}},
-//      {name: 'Population of self-employed people', identifier: {table: 4, column: 'e61_both', row: 'a65_self-employed'}},
       {
         name: 'Median monthly income',
         params: _.extend(_.clone(Indicators.queries.householdIncome, true), Indicators.queries.areaMedianModifier),
@@ -84,10 +58,6 @@ angular.module('frontendApp')
         parser: _medianMonthlyIncomeParser
       }
     ];
-
-    // init with one plot
-//    $scope.selectedIndicator = $scope.indicators[0].identifier;
-//    $scope.refresh();
 
     $scope.mapLevel = 'ca';
     $scope.theData = $scope.areaData;
