7dbad8d65a6efe6714d4e7c5891905ce38adb32b
HU Pili <hpl1989@gmail.com>
2014-02-14 10:12:13 +0800

    working api browser

--- frontend/app/scripts/controllers/browser.js
+++ frontend/app/scripts/controllers/browser.js
@@ -7,38 +7,38 @@ angular.module('frontendApp').controller('BrowserCtrl', ['$scope', 'CensusAPI', 
   // Available options
   $scope.options = {};
 
-  $scope.skip = 0;
-  $scope.count = 20; 
-
   // Build the query from the model filters
   $scope.refresh = function() {
     var q = new CensusAPI.Query($scope.model);
-    q.addParam('return', 'options,data');
-    q.addParam('projector', 'area,value')
-    q.addParam('skip', $scope.skip);
-    q.addParam('count', $scope.count);
+    q.addParam('return', 'options');
 
     q.fetch().then(function(data) {
       $scope.options = data.options;
       $scope.meta = data.meta;
-      $scope.data = CensusAPI.joinData(data.data);
-      console.log($scope.data);
+      console.log('data');
+      console.log(data);
 
-    });
-  };
-
-
-  $scope.tableParams = new ngTableParams({
+      $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
         count: 10           // count per page
       }, {
-        total: , // length of data
+        total: data.meta.length, // length of data
         getData: function($defer, params) {
           $scope.skip = (params.page() - 1) * params.count();
           $scope.count = params.count();
-          $defer.resolve($scope.data);
+          var current_view_q = new CensusAPI.Query($scope.model);
+          current_view_q.addParam('return', 'options,data');
+          current_view_q.addParam('projector', 'region,district,area,table,row,column,value');
+          current_view_q.addParam('skip', $scope.skip);
+          current_view_q.addParam('count', $scope.count);
+          current_view_q.fetch().then(function(data){
+            $defer.resolve(CensusAPI.joinData(data.data));
+          })
         }
       });
+
+    });
+  };
 
   // Clear all selections, or the selection for a single model field
   $scope.clear = function(field) {
